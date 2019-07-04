8c3fbee7230f4fd5bb4c701dc63bd73f301b3b62
Damir Samatov <princeofagility@gmail.com>
2019-07-04 15:56:52 +0300

    update ng version

---
+++ webUI/src/app/ip-routes/ip-routes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ip-routes',
+  templateUrl: './ip-routes.component.html',
+  styleUrls: ['./ip-routes.component.css']
+})
+export class IpRoutesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ webUI/src/app/vps-management/vps-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vps-management',
+  templateUrl: './vps-management.component.html',
+  styleUrls: ['./vps-management.component.css']
+})
+export class VpsManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
