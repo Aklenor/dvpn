541de1ec0a73a4a738810f91912a0d4bc152fa94
Damir Samatov <princeofagility@gmail.com>
2019-08-04 13:06:23 +0300

    prod env fixed3

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.dvpn.gib:5000/',
-  zabbixUrl: 'http://zabbix.dvpn.gib:10051'
+  apiUrl: 'http://10.1.1.243:5000/',
+  zabbixUrl: 'http://10.1.1.243:10051'
 };
